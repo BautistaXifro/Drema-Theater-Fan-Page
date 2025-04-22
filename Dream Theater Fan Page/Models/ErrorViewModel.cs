66cbfebbcc7e04176d207f024ee81ba92eb75969
MH <MH@DESKTOP-ASLJRN4>
2025-04-22 20:27:19 -0300

    Agregar archivos de proyecto.

---
+++ Dream Theater Fan Page/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dream_Theater_Fan_Page.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
