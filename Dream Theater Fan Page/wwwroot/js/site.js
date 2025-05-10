3746b45b2c7e1b901adf804e12b4815e0a74fafc
bautista xifro <bauxifro@gmail.com>
2025-05-10 15:13:50 -0300

    Agrego pagina de SHOWS y cambio vistas parciales

--- Dream Theater Fan Page/wwwroot/js/site.js
+++ Dream Theater Fan Page/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function mostrarModal(titulo, descripcion, items) {
+    document.getElementById("infoModalLabel").innerText = titulo;
+    document.getElementById("modalDescription").innerText = descripcion;
+
+    const lista = document.getElementById("modalList");
+    lista.innerHTML = "";
+
+    items.forEach(item => {
+        const li = document.createElement("li");
+    li.textContent = item;
+    lista.appendChild(li);
+    });
+
+    const modal = new bootstrap.Modal(document.getElementById("infoModal"));
+    modal.show();
+}
